ae5ccf5b2434a7490eedc635bb817383dc7d7488
sobolak <sobolak17@o2.pl>
2022-04-24 20:18:30 +0200

    fex fix

--- scrapping/avenue.py
+++ scrapping/avenue.py
@@ -8,6 +8,7 @@ from selenium.webdriver.chrome.options import Options
 from datetime import date, datetime, timedelta
 import db_connection as db
 
+avenue_hrefs = {'https://www.weatheravenue.com/pl/europe/pl/krakow/plaszow-hourly.html':'płaszów'}
 
 def get_driver():
     chrome_options = Options()
@@ -41,22 +42,25 @@ def main(hrefs_dict):
                     hour.replace("0","")
                 if hour == "00" or hour == "01":
                     day_bonus +=1
-                db.db_delete(
-                    str(selected_day + timedelta(days=day_bonus)),
-                    hour, 
-                    hrefs_dict.get(link),
-                    "avenue")
-                db.db_insert(
-                    x[2][0:4].replace("°C_","").replace("°C",""),
-                    x[4][0:2].replace("_",""),
-                    x[6].replace("%",""),
-                    x[3][0:5].replace("_","").replace("m",""), 
-                    x[5].replace("%",""),
-                    str(datetime.now())[:-7] , 
-                    str(selected_day + timedelta(days=day_bonus)), 
-                    hour, 
-                    hrefs_dict.get(link),
-                    "avenue")
+                try:
+                    db.db_delete(
+                        str(selected_day + timedelta(days=day_bonus)),
+                        hour, 
+                        hrefs_dict.get(link),
+                        "avenue")
+                    db.db_insert(
+                        x[2][0:4].replace("°C_","").replace("°C",""),
+                        x[4][0:2].replace("_",""),
+                        x[6].replace("%",""),
+                        x[3][0:5].replace("_","").replace("m",""), 
+                        x[5].replace("%",""),
+                        str(datetime.now())[:-7] , 
+                        str(selected_day + timedelta(days=day_bonus)), 
+                        hour, 
+                        hrefs_dict.get(link),
+                        "avenue")
+                except:
+                    pass
         day_bonus = 0
         for even_buffor in hours_tables:
             table = even_buffor.find_all("tr", class_ = "even")
@@ -68,20 +72,23 @@ def main(hrefs_dict):
                     hour.replace("0","")
                 if hour == "00" or hour == "01":
                     day_bonus +=1
-                db.db_delete(
-                    str(selected_day + timedelta(days=day_bonus)),
-                    hour, 
-                    hrefs_dict.get(link),
-                    "avenue")
-                db.db_insert(
-                    x[2][0:4].replace("°C_","").replace("°C",""),
-                    x[4][0:2].replace("_",""),
-                    x[6].replace("%",""),
-                    x[3][0:5].replace("_","").replace("m",""), 
-                    x[5].replace("%",""),
-                    str(datetime.now())[:-7] , 
-                    str(selected_day + timedelta(days=day_bonus)), 
-                    hour, 
-                    hrefs_dict.get(link),
-                    "avenue")
+                try:
+                    db.db_delete(
+                        str(selected_day + timedelta(days=day_bonus)),
+                        hour, 
+                        hrefs_dict.get(link),
+                        "avenue")
+                    db.db_insert(
+                        x[2][0:4].replace("°C_","").replace("°C",""),
+                        x[4][0:2].replace("_",""),
+                        x[6].replace("%",""),
+                        x[3][0:5].replace("_","").replace("m",""), 
+                        x[5].replace("%",""),
+                        str(datetime.now())[:-7] , 
+                        str(selected_day + timedelta(days=day_bonus)), 
+                        hour, 
+                        hrefs_dict.get(link),
+                        "avenue")
+                except:
+                    pass
     print("AVENUE DONE")
